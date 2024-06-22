29aee09c15c6cbddc8becca5308f4048bc2ce2eb
Khang Nguyễn <thinhkhang97@gmail.com>
2024-06-22 20:30:06 +0700

    update authentication flow

--- src/todo/dtos/create-todo.dto.ts
+++ src/todo/dtos/create-todo.dto.ts
@@ -1,7 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, MinLength } from 'class-validator';
 
 export class CreateTodoDto {
   @IsString()
   @MinLength(3)
+  @ApiProperty()
   title: string;
 }
